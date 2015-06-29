a34eda731c474f1220d4eb6673eb0fcba40dd2ab
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2015-06-29 14:58:18 +0100

    parse basic attributes of experiment: accession, total-experiments, total-samples, total-assays and description

--- parser.el
+++ parser.el
@@ -1,17 +1,40 @@
-
+;; Bunch of parsers
 (provide 'parser)
 
 (require 'download)
 (require 'xml)
 
 
+(defun get-experiment-array-express (accession)
+  "Returns experiment data identified by ACCESSION as a hashmap from ArrayExpress."
+  (let* ((output (call-array-express accession 'experiments))
+	 (root (with-temp-buffer
+		 (insert output)
+		 (xml-parse-region (point-min) (point-max))))
+	 (to-return nil)
+	 (experiments (car root))
+	 (experiments-attrs (xml-node-attributes experiments))
+	 (total-attr (cdr (assq 'total experiments-attrs)))
+	 (total-samples-attr (cdr (assq 'total-samples experiments-attrs)))
+	 (total-assays-attr (cdr (assq 'total-assays experiments-attrs)))
+	 (experiment-node (car (xml-get-children experiments 'experiment)))
+	 (description-node (car (xml-get-children experiment-node 'description)))
+	 (description-text (first (xml-node-children (car (xml-get-children description-node 'text)))))
+	 (sampleattribute-node (xml-get-children experiment-node 'sampleattribute)))
+    (push (cons "accession" accession) to-return )
+    (push (cons "total-experiments" total-attr) to-return)
+    (push (cons "total-smaples" total-samples-attr) to-return)
+    (push (cons "total-assays" total-assays-attr) to-return)
+    (push (cons "description" description-text) to-return)
+    ))
 
-(xml-parse (xml-string object-parser)
-"Parse xml from XML-STRING using function OBJECT-PARSER.
-That function should accepts .... as input"
-(let* ((root (with-temp-buffer
-	      (insert xml-string)
-	      (xml-parse-region (point-min) (point-max))))
-       ())
-  ())
-  )
+
+(defun --sampleattribute-nodes-parser (sampleattribute-nodes)
+  "Returns sampleattribute list"
+  (let ((node-parsed nil))
+    (loop for node in sampleattribute-nodes
+	  for category = (first (xml-node-children (car (xml-get-children node 'category))))
+	  for value = (xml-get-children node 'value)
+	  do (message "type: %s   " (type-of category))
+	  collect (cons category value)
+	  )))
--- test/test.el
+++ test/test.el
@@ -2,7 +2,7 @@
 (provide 'test)
 
 (require 'f)
-
+(require 'ert)
 
 (defconst test-source-path
   (f-parent (f-this-file)))
@@ -12,28 +12,21 @@
 
 ;; simplest sollution
 (require 'download (f-expand "download.el" source-path))
+(require 'parser (f-expand "parser.el" source-path))
 
 ;; test initialising taken from article http://rejeep.github.io/emacs/testing/cask/ert-runner/2013/09/26/unit-testing-in-emacs.html
+;; not yet
 
-(prin1 *data-sources*)
-
-;; take all dirs apart .git
-(f-directories source-path (lambda (x) (null (string-match "\\.git" x))))
-
-(> 5 0)
-
-(directory-files source-path t ".el$")
-(let (())
-  )
-(load (concat (pwd) "download.el"))
-
-
-
-(ert-deftest get-sequence ()
+(ert-deftest test-get-sequence ()
   "Take experiment data from the external server."
-(message "output: !!!%s" (buffer-name (call-array-express "E-MEXP-130" 'experiments)))
-)
+  (let ((out-text (call-array-express "E-MEXP-130" 'experiments)))
+    (message "length: %s" (length out-text))
+    (should (> (length out-text) 0))))
 
-(test)
 
-(message "dir:%s" (pwd))
+(ert-deftest test-get-nodes ()
+  "Take nodes from experiment 'E-MEXP-130'"
+  (message "^^^^^^^^^^^^^^^^^^^^^^")
+  (let ((experiment (get-experiment-array-express "E-MEXP-130")))
+    (message "experiment node: %s\n" experiment)
+    (should (listp experiment))))

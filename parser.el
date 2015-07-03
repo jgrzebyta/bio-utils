656b0c90e29187859363990170f1c687d0db8271
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2015-07-03 10:38:21 +0100

    convert parser outcome to hashtable-based structure

--- parser.el
+++ parser.el
@@ -6,13 +6,68 @@
 (require 'cl)
 
 
+
+
+
+(defun --sampleattribute-nodes-parser (sampleattribute-nodes)
+  "Returns sampleattribute list"
+  (let ((to-return (make-hash-table :test 'equal)))
+    (loop for node in sampleattribute-nodes
+	  for category = (first (xml-node-children (car (xml-get-children node 'category))))
+	  for value-nodes = (xml-get-children node 'value)
+	  for value = (loop for v in value-nodes
+			    for v-value = (first (xml-node-children v))
+			    collect v-value
+			    )
+	  do (puthash category value to-return)
+	  )
+    to-return))
+
+
+(defun --experimental-factor-parser (experimental-factor-node)
+  "Parse experimental factor description from EXPERIMENAL-FACTOR-NODE."
+  (let ((to-return (make-hash-table))
+	(name (first (xml-node-children (car (xml-get-children experimental-factor-node 'name)))))
+	(value-nodes (loop for v in (xml-get-children experimental-factor-node 'value)
+			   collect (first (xml-node-children v)))))
+    (puthash 'name name to-return)
+    (puthash 'values value-nodes to-return)
+    to-return
+    ))
+
+
+(defun --bibliography-parser (experimental-factor-node)
+  "Parse bibliography record from EXPERIMENAL-FACTOR-NODE."
+  (let* ((to-return (make-hash-table))
+	 (bibliography-node (car (xml-get-children experimental-factor-node 'bibliography)))
+	 (accession (car (xml-node-children (car (xml-get-children bibliography-node 'accession)))))
+	 (authors (car (xml-node-children (car (xml-get-children bibliography-node 'authors)))))
+	 (title (car (xml-node-children (car (xml-get-children bibliography-node 'title)))))
+	 (issue (car (xml-node-children (car (xml-get-children bibliography-node 'issue)))))
+	 (pages (car (xml-node-children (car (xml-get-children bibliography-node 'pages)))))
+	 (publication (car (xml-node-children (car (xml-get-children bibliography-node 'publication)))))
+	 (volume (car (xml-node-children (car (xml-get-children bibliography-node 'volume)))))
+	 (year (car (xml-node-children (car (xml-get-children bibliography-node 'year))))))
+    (puthash 'accession accession to-return)
+    (puthash 'authors authors to-return)
+    (puthash 'title title to-return)
+    (puthash 'issue issue to-return)
+    (puthash 'pages pages to-return)
+    (puthash 'publication publication to-return)
+    (puthash 'volume volume to-return)
+    (puthash 'year year to-return)
+    to-return
+    ))
+
+
+
 (defun get-experiment-array-express (accession)
   "Returns experiment data identified by ACCESSION as a hashmap from ArrayExpress."
   (let* ((output (call-array-express accession 'experiments))
 	 (root (with-temp-buffer
 		 (insert output)
 		 (xml-parse-region (point-min) (point-max))))
-	 (to-return nil)
+	 (to-return (make-hash-table))
 	 (experiments (car root))
 	 (experiments-attrs (xml-node-attributes experiments))
 	 (total-attr (cdr (assq 'total experiments-attrs)))
@@ -25,59 +80,14 @@
 	 (sampleattribute-nodes (xml-get-children experiment-node 'sampleattribute))
 	 (name (first (xml-node-children (car (xml-get-children experiment-node 'name))))))
     (message "name: %S" name)
-    (push (cons "accession" accession) to-return )
-    (push (cons "name" name) to-return)
-    (push (cons "total-experiments" total-attr) to-return)
-    (push (cons "total-smaples" total-samples-attr) to-return)
-    (push (cons "total-assays" total-assays-attr) to-return)
-    (push (cons "experimental-factor" (--experimental-factor-parser experimental-factor-node)) to-return)
-    (push (cons "sample-attribute" (--sampleattribute-nodes-parser sampleattribute-nodes)) to-return)
-    (push (cons "description" description-text) to-return)
-    (push (cons "bibiography" --bibliography-parser) to-return)
+    (puthash 'accession accession to-return )
+    (puthash 'name name to-return)
+    (puthash 'total-experiments total-attr to-return)
+    (puthash 'total-smaples total-samples-attr to-return)
+    (puthash 'total-assays total-assays-attr to-return)
+    (puthash 'experimental-factor (--experimental-factor-parser experimental-factor-node) to-return)
+    (puthash 'sample-attribute (--sampleattribute-nodes-parser sampleattribute-nodes) to-return)
+    (puthash 'description description-text to-return)
+    (puthash 'bibliography (--bibliography-parser experiment-node) to-return)
+    to-return
     ))
-
-
-(defun --sampleattribute-nodes-parser (sampleattribute-nodes)
-  "Returns sampleattribute list"
-  (let ((node-parsed nil))
-    (loop for node in sampleattribute-nodes
-	  for category = (first (xml-node-children (car (xml-get-children node 'category))))
-	  for values = (xml-get-children node 'value)
-	  for value = nil
-	  do (progn
-	      (loop for v in values
-		   for v-value = (first (xml-node-children v))
-		   do (push v-value value))
-	      (reverse value))
-	  collect (cons category value)
-	  )))
-
-
-(defun --experimental-factor-parser (experimental-factor-node)
-  "Parse experimental factor description from EXPERIMENAL-FACTOR-NODE."
-  (let ((name (first (xml-node-children (car (xml-get-children experimental-factor-node 'name)))))
-	(value-nodes (loop for v in (xml-get-children experimental-factor-node 'value)
-			   collect (first (xml-node-children v)))))
-    (concat "\"" name "\"" " with variants: " (mapconcat (lambda (x) (format "%s" x)) value-nodes ", "))))
-
-
-(defun --bibliography-parser (experimental-factor-node)
-  "Parse bibliography record from EXPERIMENAL-FACTOR-NODE."
-  (let ((bibliography-node (car (xml-get-children experimental-factor-node 'bibliography)))
-	(accession (car (xml-node-children (xml-get-children bibliography-node 'accession))))
-	(authors (car (xml-node-children (xml-get-children bibliography-node 'authors))))
-	(title (car (xml-node-children (xml-get-children bibliography-node 'title))))
-	(issue (car (xml-node-children (xml-get-children bibliography-node 'issue))))
-	(pages (car (xml-node-children (xml-get-children bibliography-node 'pages))))
-	(publication (car (xml-node-children (xml-get-children bibliography-node 'publication))))
-	(volume (car (xml-node-children (xml-get-children bibliography-node 'volume))))
-	(year (car (xml-node-children (xml-get-children bibliography-node 'year)))))
-    (list (cons "accession" accession)
-	  (cons "authors" authors)
-	  (cons "title" title)
-	  (cons "issue" issue)
-	  (cons "pages" pages)
-	  (cons "publication" publication)
-	  (cons "volume" volume)
-	  (cons "year" year))))
-
--- test/test.el
+++ test/test.el
@@ -14,33 +14,43 @@
 (require 'download (f-expand "download.el" source-path))
 (require 'parser (f-expand "parser.el" source-path))
 
+(defun get-experiment ()
+  "Get E-MEXP-130 experiment record"
+  (get-experiment-array-express "E-MEXP-130"))
+
+
 ;; test initialising taken from article http://rejeep.github.io/emacs/testing/cask/ert-runner/2013/09/26/unit-testing-in-emacs.html
 ;; not yet
 
-(ert-deftest test-get-sequence ()
-  "Take experiment data from the external server."
-  (let ((out-text (call-array-express "E-MEXP-130" 'experiments)))
-    (message "length: %s" (length out-text))
-    (should (> (length out-text) 0))))
 
-
-;; (ert-deftest test-get-nodes ()
-;;   "Take nodes from experiment 'E-MEXP-130'"
-;;   (message "^^^^^^^^^^^^^^^^^^^^^^")
-;;   (let ((experiment (get-experiment-array-express "E-MEXP-130")))
-;;     (message "experiment node: %s\n" experiment)
-;;     (should (listp experiment))))
+(ert-deftest test-get-nodes ()
+  "Take nodes from experiment 'E-MEXP-130'"
+  (message "^^^^^^^^^^^^^^^^^^^^^^")
+  (let ((experiment (get-experiment)))
+    (message "experiment node: %S\n" experiment)
+    (should (hash-table-p experiment))))
 
 
 (ert-deftest test-sampleattributes ()
   "Test parsing of sampleattributes nodes"
-  (message "&&&&&&&&&&&&&&&&")
-  (let* ((experiment (get-experiment-array-express "E-MEXP-130"))
-	(sattr (assoc "sample-attribute" experiment)))
-    (message "sample-attribute node: %S\n" (assoc "sample-attribute" experiment))
-    (should (listp experiment)))
+  (let* ((experiment (get-experiment))
+	(sattr (gethash 'sample-attribute experiment)))
+    (message "sample-attribute node: %S\n" sattr)
+    (should (hash-table-p experiment))
+    (should (member "StrainOrLine" (loop for v being hash-keys of sattr collect v))))
   )
 
 (ert-deftest test-bibliography ()
   "Test parsing bibliography"
-  )
+  (message "test-bibliography &&&&&&&&&&&&&&&&&&&\n")
+  (let* ((experiment (get-experiment))
+	 (bibliography (gethash 'bibliography experiment))
+	 (bibliography-keys (loop for k being hash-keys of bibliography collect k))
+	 )
+    (message "bibliography: %S\n" bibliography)
+    (message "keys of experiment: %S" (loop for k being hash-keys of experiment collect k))
+    (message "bibliography hash keys: %S" bibliography-keys)
+    (should (member 'bibliography (loop for k being hash-keys of experiment collect k)))
+    (should (hash-table-p bibliography))
+    (should (member 'authors bibliography-keys))
+    ))

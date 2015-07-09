bfa1dc53249e2a46c0526660304f4fbe803d3496
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2015-07-09 16:24:37 +0100

    add features rich parser

--- test.lisp
+++ test.lisp
@@ -1,9 +1,15 @@
 (require 'closure-html)
 (require 'drakma)
+(require 'cxml-stp) ;; DOM parser, features rich
+
+(defconstant *content-class* "content-column")
+(defconstant *content-element* "div")
+
 
 (let* ((id "YBL045C")
        (base-url (format nil "http://www.yeastgenome.org/search?query=~A" id))
        (page (drakma:http-request base-url))
-       (page-parsed (chtml:parse page (chtml:make-string-sink))))
-  (format t "page:~%~s~%" (type-of page-parsed))
+       (page-parsed (chtml:parse page (stp:make-builder))))
+  (format t "URL request: ~s~%" base-url)
+  (format t "output-type: ~s~%" (type-of page-parsed))
 )

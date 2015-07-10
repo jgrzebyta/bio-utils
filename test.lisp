cf9e462879713da2cb27d973770b67efb3054a7e
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2015-07-10 01:31:59 +0100

    further improvements of test.list stran's parser

--- test.lisp
+++ test.lisp
@@ -1,15 +1,28 @@
+
 (require 'closure-html)
 (require 'drakma)
 (require 'cxml-stp) ;; DOM parser, features rich
 
-(defconstant *content-class* "content-column")
-(defconstant *content-element* "div")
+(defparameter content-class "content-column")
+(defparameter content-element "div")
 
 
-(let* ((id "YBL045C")
-       (base-url (format nil "http://www.yeastgenome.org/search?query=~A" id))
+(let* ((id "ybl045c")
+       (base-url (format nil "http://www.yeastgenome.org/search?query=~a" id))
        (page (drakma:http-request base-url))
        (page-parsed (chtml:parse page (stp:make-builder))))
-  (format t "URL request: ~s~%" base-url)
+  (format t "url request: ~s~%" base-url)
   (format t "output-type: ~s~%" (type-of page-parsed))
+  (stp:do-recursively (div page-parsed)
+    ;;(format t "div item: ~S~%" div)
+    (format t "is element: ~s~%" (typep div 'stp:element))
+    (format t "has class: ~s~%" (equal (stp:local-name div) content-element))
+    (when (and (typep div 'stp:element)
+	       (equal (stp:local-name div) content-element)
+	       ;;(search content-class (stp:attribute-value div content-element))
+	       )
+      (format t "element: ~S~%" (stp:attribute-value div "class")))
+    )
 )
+
+

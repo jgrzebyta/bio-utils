744dbbcddb97cc2bc136dde762ee7494707456bb
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2015-07-12 02:26:46 +0100

    clean sgd-parser.lisp file - remove test code

--- sgd-parser.lisp
+++ sgd-parser.lisp
@@ -1,14 +1,20 @@
-;; useful for QuickLoad for manual launching 
+
+
+;; Saccharomyces Genome Database web page parser
+;; This parser collects information from yeast strain web page and converts it into useful hashmap.
+;;
+;; Useful for QuickLoad for manual launching 
 ;; (ql:quickload 'drakma)
 ;; (ql:quickload 'cl-html5-parser)
 ;; (ql:quickload 'cl-ppcre)
 ;; (ql:quickload 'st-json)
 
-
 (require 'drakma)
 (require 'cl-html5-parser)
 (require 'cl-ppcre)
 (require 'st-json)
+
+
 
 (defun -get-bootstrap-json-as-string (in-string)
   "Extract value of bootstappedData from IN-STRING and return as string."
@@ -22,15 +28,17 @@
       (elt groups 0)  ;; unpack data from vector
       )))
 
+(defun -get-json-as-hashmap (json-object)
+  "Converts internal type of JSO instance JSON-OBJECT into custom hashmap"
+  
+  )
 
-(let* ((id "ybl045c")
-       (base-url (format nil "http://www.yeastgenome.org/search?query=~a" id))
-       (page (drakma:http-request base-url))
-       (json-string (-get-bootstrap-json-as-string page))
-       (json-object (st-json:read-json-from-string json-string)))
-  (format t "url request: ~s~%" base-url)
-  (format t "json-string: \"~a\"~%" json-object)
-
-)
+(defun get-sgd (name)
+  "Return Saccharomyces Genome Database record identified by NAME as a hashmap."
+  (let* ((base-url (format nil "http://www.yeastgenome.org/search?query=~a" name))
+	 (page (drakma:http-request base-url))
+	 (json-string (-get-bootstrap-json-as-string page))
+	 (json-object (st-json:read-json-from-string json-string)))
+    (-get-json-as-hashmap json-object)))
 
 

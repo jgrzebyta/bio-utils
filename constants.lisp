1a7a4f21120c45660ca968d453f5262f92f9f3dd
Jacek Grzebyta <jgrzebyta@users.noreply.github.com>
2015-10-04 23:09:57 +0100

    adds two uniprot-search-* predefined queries

--- constants.lisp
+++ constants.lisp
@@ -2,6 +2,8 @@
 (in-package #:bio-utils)
 
 (defparameter *data-sources* '(("uniprot" . "http://www.uniprot.org/uniprot/~a.~a")
+			       ("uniprot-search-tab" . "http://www.uniprot.org/uniprot/?search=~a&format=tab&columns=id,entry+name,reviewed,protein+names,genes,organism,organism-id")
+			       ("uniprot-search-rdf" . "http://www.uniprot.org/uniprot/?search=~a&format=rdf")
 			       ("ebi-eye" . "http://www.ebi.ac.uk/ebisearch/ws/rest/~a?query=~a")
 			       ("array-express" . "http://www.ebi.ac.uk/arrayexpress/xml/v2/~a?accession=~a")
 			       ("yeast-genome-search" . "http://www.yeastgenome.org/search?query=~a"))
